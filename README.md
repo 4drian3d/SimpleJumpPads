6f210e529ec55651219022194c9c11af3883f2fe
Adrian <adriangonzalesval@gmail.com>
2023-06-29 10:17:14 -0500

    feat: Added 1.20.x Support
    Removed 1.19.3 support

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Simple JumpPads plugin, optimized for the latest version of Paper, Pufferfish an
 
 ## Requirements
 - Java 17
-- Paper, Pufferfish or Purpur 1.19.3+
+- Paper, Pufferfish or Purpur 1.19.4+
 
 ## Features
 - Configure multiple types of materials for your JumpPads (compatible with all types of pressure plates)
--- src/main/java/io/github/_4drian3d/simplejumppads/JumpPadsBootstrap.java
+++ src/main/java/io/github/_4drian3d/simplejumppads/JumpPadsBootstrap.java
@@ -1,18 +1,20 @@
 package io.github._4drian3d.simplejumppads;
 
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
 import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-@SuppressWarnings("UnstableApiUsage")
+@SuppressWarnings({"UnstableApiUsage", "unused"})
 public class JumpPadsBootstrap implements PluginBootstrap {
+
     @Override
-    public void bootstrap(@NotNull PluginProviderContext context) {
+    public void bootstrap(final @NotNull BootstrapContext context) {
     }
 
     @Override
-    public @NotNull JavaPlugin createPlugin(@NotNull PluginProviderContext context) {
+    public @NotNull JavaPlugin createPlugin(final @NotNull PluginProviderContext context) {
         return new SimpleJumpPads(context.getDataDirectory());
     }
 }
