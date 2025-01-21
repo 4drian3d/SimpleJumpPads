a7841545150625636f3043153058978eb62a1da1
Adrian <adriangonzalesval@gmail.com>
2025-01-21 00:06:40 -0500

    refactor: Refactored plugin command and listener to use direct reference instead of reflection/asm

--- src/main/java/io/github/_4drian3d/simplejumppads/JumpPadsBootstrap.java
+++ src/main/java/io/github/_4drian3d/simplejumppads/JumpPadsBootstrap.java
@@ -1,20 +1,56 @@
 package io.github._4drian3d.simplejumppads;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.github._4drian3d.simplejumppads.configuration.ConfigurationContainer;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
 import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 @SuppressWarnings({"UnstableApiUsage", "unused"})
-public class JumpPadsBootstrap implements PluginBootstrap {
+public final class JumpPadsBootstrap implements PluginBootstrap {
+    private ConfigurationContainer configurationContainer;
 
     @Override
     public void bootstrap(final @NotNull BootstrapContext context) {
+        this.configurationContainer = ConfigurationContainer.fromFile(context.getDataDirectory().resolve("config.conf"));
+        context.getLifecycleManager().registerEventHandler(
+                LifecycleEvents.COMMANDS,
+                event -> event.registrar()
+                        .register(context.getPluginMeta(), commandFromContext(context), "SimpleJumpPads Command", List.of("sj")));
     }
 
     @Override
     public @NotNull JavaPlugin createPlugin(final @NotNull PluginProviderContext context) {
-        return new SimpleJumpPads(context.getDataDirectory());
+        return new SimpleJumpPads(this.configurationContainer);
+    }
+
+    private LiteralCommandNode<CommandSourceStack> commandFromContext(BootstrapContext context) {
+      return Commands.literal("simplejumppads")
+              .executes(ctx -> {
+                ctx.getSource().getSender().sendRichMessage("<rainbow>SimpleJumpPads | by 4drian3d");
+                return Command.SINGLE_SUCCESS;
+              })
+              .requires(ctx -> ctx.getSender().hasPermission("simplejumppads.command"))
+              .then(Commands.literal("reload")
+                      .executes(ctx -> {
+                        try {
+                          configurationContainer.load();
+                          ctx.getSource().getSender().sendRichMessage("<green>Correctly reloaded");
+                        } catch (Exception e) {
+                          ctx.getSource().getSender().sendRichMessage("<red>An error occurred on configuration reload, check console");
+                          context.getLogger().error("An error occurred loading configuration", e);
+                        }
+                        return Command.SINGLE_SUCCESS;
+                      })
+              )
+              .build();
     }
 }
--- src/main/java/io/github/_4drian3d/simplejumppads/listener/JumpPadInteractListener.java
+++ src/main/java/io/github/_4drian3d/simplejumppads/listener/JumpPadInteractListener.java
@@ -5,35 +5,34 @@ import net.kyori.adventure.sound.Sound;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.EventExecutor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public final class JumpPadInteractListener implements Listener {
-    private final SimpleJumpPads plugin;
+public record JumpPadInteractListener(SimpleJumpPads plugin) implements EventExecutor, Listener {
 
-    public JumpPadInteractListener(final SimpleJumpPads plugin) {
-        this.plugin = plugin;
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    public void onInteract(final PlayerInteractEvent event) {
-        if (event.getAction() != Action.PHYSICAL) {
+    @Override
+    public void execute(final @NotNull Listener listener, final @NotNull Event event) {
+        final PlayerInteractEvent interactEvent = (PlayerInteractEvent) event;
+        if (interactEvent.getAction() != Action.PHYSICAL) {
             return;
         }
 
-        final Player player = event.getPlayer();
+        final Player player = interactEvent.getPlayer();
         if (!player.hasPermission("simplejumppads.jump")) {
             return;
         }
 
         final Location location = copy(player.getLocation());
-        final Material type = Objects.requireNonNull(event.getClickedBlock()).getType();
+        final Material type = Objects.requireNonNull(interactEvent.getClickedBlock()).getType();
 
-        for (final var section : plugin.getConfiguration().getSections()) {
+        for (final var section : plugin.getConfiguration().get().getSections()) {
             if (Objects.equals(section.getMaterial(), type)) {
                 player.setVelocity(
                         location.getDirection()
@@ -61,5 +60,10 @@ public final class JumpPadInteractListener implements Listener {
                 original.getYaw(),
                 original.getPitch()
         );
+    }
+
+    public void register() {
+        plugin.getServer().getPluginManager()
+                .registerEvent(PlayerInteractEvent.class, this, EventPriority.NORMAL, this, plugin, true);
     }
 }
