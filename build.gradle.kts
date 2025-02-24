702edd8d49a2fa46b1e560f8fe36f6b1dede8ca9
Adrian <adriangonzalesval@gmail.com>
2025-02-24 15:20:55 -0500

    feat: Removed library loading from Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     alias(libs.plugins.runpaper)
+    alias(libs.plugins.shadow)
 }
 
 repositories {
@@ -9,20 +10,39 @@ repositories {
 
 dependencies {
     compileOnly(libs.paper)
-    compileOnly(libs.configurate)
-    compileOnly(libs.adventure.serializer)
+    compileOnly(libs.configurate.core)
+    implementation(libs.configurate.hocon) {
+        isTransitive = false
+    }
+    implementation(libs.adventure.serializer) {
+        isTransitive = false
+    }
+    implementation(libs.hocon)
 }
 
 tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        archiveBaseName.set(rootProject.name)
+        archiveClassifier.set("")
+        relocate("org.spongepowered.configurate.hocon",
+            "io.github._4drian3d.simplejumppads.libs.configurate.hocon")
+        relocate("com.typesafe", "io.github._4drian3d.simplejumppads.libs.typesafe")
+        relocate("net.kyori.adventure.serializer.configurate4",
+            "io.github._4drian3d.simplejumppads.libs.configurate-serializer")
+        minimize()
+    }
     compileJava {
         options.release.set(21)
     }
     runServer {
-        minecraftVersion("1.19.4")
+        minecraftVersion("1.21.4")
     }
     processResources {
         filesMatching("paper-plugin.yml") {
-            expand("version" to project.version)
+            expand("version" to version)
         }
     }
 }
